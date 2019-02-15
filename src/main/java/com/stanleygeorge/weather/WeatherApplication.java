b306f50de23dd6f81eb22dc12c037db002d15900
stangeorge <stanleygeorge@gmail.com>
2019-02-15 12:10:52 -0600

    Refactored to modularize code. Updated unit tests

--- src/main/java/com/stanleygeorge/weather/WeatherApplication.java
+++ src/main/java/com/stanleygeorge/weather/WeatherApplication.java
@@ -10,35 +10,47 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @RestController
 public class WeatherApplication {
-	static String street = "";
-	static String city = "Chicago";
-	static String state = "IL";
-	static String address = "street="+street+"&city="+city+"&state="+state;
-	static String geocodeUri = "https://geocoding.geo.census.gov/geocoder/locations/address?benchmark=9&format=json&"
-			+address;
+	static String geocodeUri = "https://geocoding.geo.census.gov/geocoder/locations/address?benchmark=9&format=json&";
 	static String zoneUri = "https://api.weather.gov/points/";
 
 	@RequestMapping("/")
 	public String home() {
+		return getDetailedForecast("201+E+Randolph+St", "Chicago", "IL");
+	}
+
+	private String getDetailedForecast(String street, String city, String state) {
 		RestTemplate restTemplate = new RestTemplate();
-		//String result = restTemplate.getForObject(geocodeUri, String.class);
-		JsonNode geocode = restTemplate.getForObject(geocodeUri, JsonNode.class);
-		JsonNode coordinates = geocode.get("result").get("addressMatches").get(0).get("coordinates");
-		String yx= coordinates.get("y")+","+coordinates.get("x");
-
-		JsonNode zone = restTemplate.getForObject(zoneUri+yx, JsonNode.class);
-		String forecastUri = zone.get("properties").get("forecast").toString();
-		forecastUri = forecastUri.substring(1, forecastUri.length()-1);
-
+		String forecastUri = getForecastUri(street, city, state);
 		JsonNode forecast = restTemplate.getForObject(forecastUri, JsonNode.class);
 		String detailedForecast = forecast.get("properties").get("periods").get(0).get("detailedForecast").toString();
 		return detailedForecast;
 	}
 
+	/* Returns a comma-separated co-ordinate for a given address*/
+	 public String geocode(String street, String city, String state) {
+		String addressPararms = "street="+street+"&city="+city+"&state="+state;
 
-	public static void main(String[] args) {
+		RestTemplate restTemplate = new RestTemplate();
+		JsonNode geocode = restTemplate.getForObject(geocodeUri+addressPararms, JsonNode.class);
+		JsonNode coordinates = geocode.get("result").get("addressMatches").get(0).get("coordinates");
+		String yx= coordinates.get("y")+","+coordinates.get("x");
+		return yx;
+	}
+
+	/* Returns The Zone URI for doing the forecast*/
+	public  String getForecastUri(String street, String city, String state) {
+		String yxCoordinates = geocode(street, city, state);
+		RestTemplate restTemplate = new RestTemplate();
+		JsonNode zone = restTemplate.getForObject(zoneUri+yxCoordinates, JsonNode.class);
+		String forecastUri = zone.get("properties").get("forecast").toString();
+		forecastUri = forecastUri.substring(1, forecastUri.length()-1);
+		return forecastUri;
+	}
+
+	public  static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
 	}
+
 
 }
 
--- src/test/java/com/stanleygeorge/weather/WeatherApplicationTests.java
+++ src/test/java/com/stanleygeorge/weather/WeatherApplicationTests.java
@@ -2,6 +2,7 @@ package com.stanleygeorge.weather;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class WeatherApplicationTests {
 
+	@Autowired
+	private  WeatherApplication weatherApplication;
+
 	@Test
-	public void contextLoads() {
+	public void geocode() throws Exception {
+		String expected="41.884277,-87.62297";
+		String actual = weatherApplication.geocode("201+E+Randolph+St", "Chicago", "IL");
+		assert(expected).equals(actual);
+	}
+
+	@Test
+	public void getForecastUri() throws Exception {
+		String expected = "https://api.weather.gov/gridpoints/LOT/75,72/forecast";
+		String actual = weatherApplication.getForecastUri("201+E+Randolph+St", "Chicago", "IL");
+		assert(expected).equals(actual);
 	}
 
 }
